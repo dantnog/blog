026fb1e93f919a149dbee0b8166815be5ccad8b0
Daniel Nogueira <nogueira.ntd@gmail.com>
2022-12-30 15:45:33 -0300

    fix: Load theme on start

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 import { Styles } from './app.styles';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,13 @@ import { Styles } from './app.styles';
 export class AppComponent implements OnInit{
   s = Styles
 
-  constructor(private app: AppService) {}
+  constructor(
+    private app: AppService,
+    private theme: ThemeService
+  ) {}
 
   ngOnInit(): void {
     this.app.getUserFromLS()
+    this.theme.loadTheme()
   }
 }
